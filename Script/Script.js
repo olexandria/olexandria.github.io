482af016d2ca71f3c6f0696be9b17fcfa1b4474e
olexandria <43313943+olexandria@users.noreply.github.com>
2020-04-13 13:44:45 +0300

    Add files via upload

--- Script/Script.js
+++ Script/Script.js
@@ -1,1 +1,54 @@
-
+// Used to make some changes when scrolling
+window.onscroll = function() {myFunction(); scrollFunction()};
+// Used to change navigation bar when scrolling
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+// Used to the button "To the top" appears
+var mybutton = document.getElementById("myBtn");
+mybutton.className = "w3-hide";
+function scrollFunction() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    mybutton.className = "w3-show";
+  } else {
+    mybutton.className = "w3-hide";
+  }
+}
+// Used to scroll to the top of the document, when the user clicks on the button
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+// Used to make slide show
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); 
+}
